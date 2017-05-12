f16034a2d005a31712b284edf596998a29e5e9d2
Dan Patey <danpatey@danpatey.com>
2017-05-12 12:29:58 -0700

    Popping up a quick parse check and empty tableview

--- FitPlanJSONtoTableView/ViewController.m
+++ FitPlanJSONtoTableView/ViewController.m
@@ -20,10 +20,30 @@
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"ViewWillAppear has fired");
+    NSString *planJSON = @"http://input.fitplanapp.com/fitplan-server/v2/plans?locale=en";
+    
+    // Build the request and send it async
+    NSURL *msgURL = [NSURL URLWithString:planJSON];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask *messageTask = [session dataTaskWithURL:msgURL completionHandler:^
+                                     (NSData *data, NSURLResponse * response, NSError * error)
+        {
+            // Parse our JSON response into an array
+            NSError *parseError = nil;
+            NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data
+                                                        options:0
+                                                        error:&parseError];
+            
+            if (!parseError) {
+                NSLog(@"json array is %@", jsonArray);
+            } else {
+                NSString *err = [parseError localizedDescription];
+                NSLog(@"Encountered an error parsing: @", err);
+            }
+        }];
+    [messageTask resume];
 }
-
 
 @end
